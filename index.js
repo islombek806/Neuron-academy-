48aca35c3bd9836a6f16def23d58c189dd0e6af2
islombek806 <israelpolice30@gmail.com>
2025-07-18 00:37:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Neuron Academy</h1>
+      <p>Sun’iy intellekt kurslari onlayn sotish platformasi.</p>
+      <a href="/kurslar">Kurslar ro'yxati</a>
+    </div>
+  );
+}
